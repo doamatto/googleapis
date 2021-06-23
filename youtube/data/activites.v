a5a2a99aa743956dcbd57785acbc2f05ea11d7e0
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-06-23 13:30:48 -0700

    fix: issues and seperate params and resp into structs

--- youtube/data/activites.v
+++ youtube/data/activites.v
@@ -2,32 +2,37 @@ import json
 import net.http
 import time
 
-// Returns a list of channel activity events that match the request criteria. For more information, see https://developers.google.com/youtube/v3/docs/activities/list
-pub fn activites_list(part string, ?options []{
-	filters: {
+struct Options {
+	filters Options_Filters
+	maxResults u16
+	pageToken string
+	publishedAfter string // Must be ISO 8601
+	publishedBefore string // Must be ISO 8601 
+	regionCode string // Must be ISO 3166-1 alpha-2
+}
+
+struct Options_Filters {
 		// Can only have one filter
-		channelId: string,
-		mine: bool,
-	},
-	maxResults: u16,
-	pageToken: string,
-	// Must be ISO 8601
-	publishedAfter: string,
-	// Must be ISO 8601
-	publishedBefore: string,
-	// Must be ISO 3166-1 alpha-2
-	regionCode: string,
-}) []{
-	kind: string,
-	etag: string,
-	nextPageToken: string,
-	prevPageToken: string,
-	pageInfo: {
-		totalResults: int,
-		resultsPerPage: int,
-	},
-	items: []
-} {
+		channelId string
+		mine bool
+}
+
+struct Response {
+	kind string
+	etag string
+	nextPageToken string
+	prevPageToken string
+	pageInfo Response_PageInfo
+	items map
+}
+
+struct Response_PageInfo {
+		totalResults int
+		resultsPerPage int
+}
+
+// Returns a list of channel activity events that match the request criteria. For more information, see https://developers.google.com/youtube/v3/docs/activities/list
+pub fn activites_list(part string, options? Options) Response {
 	url := "https://www.googleapis.com/youtube/v3/activites?part=$part"
 	if (filters != nil) {
 		// Can only have one filter
@@ -48,8 +53,8 @@ pub fn activites_list(part string, ?options []{
 	}
 	if(options.regionCode) { url = "$url&regionCode=$options.regionCode" }
 
-	resp := http.get(url) or { return err }
-	body := json.decode(resp.text) or { return err }
+	resp := http.get_text(url) or { return err }
+	mut body := json.decode(resp) or { return err }
 	
 	res := body.as_map()
 	return res
