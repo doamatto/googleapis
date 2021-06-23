97f032560a30a5dbb36838bdab0fc9ce85329b7a
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-06-23 12:33:07 -0700

    chore: add videoCategories (#1)

---
+++ youtube/data/videoCategories.v
@@ -0,0 +1,37 @@
+import net.http
+
+// Returns a list of categories that can be associated with YouTube videos. For more information, see https://developers.google.com/youtube/v3/docs/videoCategories/list
+pub fn list(part string, options ?[]{
+	filters: {
+		id: string,
+		regionCode: string,
+	},
+	hl: string,
+}) []{
+	kind: string,
+	etag: string,
+	nextPageToken: string,
+	prevPageToken: string,
+	pageInfo: {
+		totalResults: int,
+		resultsPerPage: int,
+	},
+	items: [],
+} {
+	url := "https://www.googleapis.com/youtube/v3/videoCategories?part=$part"
+	if (filters != nil) {
+		// Can only have one filter
+		if (filters.id && filters.regionCode) { return err }
+		if (filters.id) { url += "&id=$filters.id" }
+		if (filters.regionCode) { url += "&regionCode=$filters.regionCode" }
+	}  
+
+	if(options.hl) { url += "&hl=$options.hl" }
+
+	resp := http.get(url) or { return err }
+	body := json.decode(resp.text) or { return err }
+	
+	res := body.as_map()
+	return res
+
+}
